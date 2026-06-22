02a55287b3202d6bd849066afd88f584437ea620
InigoPeLo <inigopl69@gmail.com>
2026-06-22 16:42:40 +0200

    Modified the function for arbitrary n qbits problem, insted of only 2 qbits

--- circuito.py
+++ circuito.py
@@ -1,19 +1,23 @@
 import pennylane as qml
 from pennylane import numpy as np 
 
-dev=qml.device("default.qubit", wires=2)
+#Con esto podemos formar el circuito según las necisidades del problema
+def make_circuit(n_qubits):
+    dev=qml.device("default.qubit", wires=n_qubits)
 
-@qml.qnode(dev)
+    @qml.qnode(dev)
 
-def circuito(x, theta):
-    l=len(theta)
-    #Encoding
-    qml.RY(x[0], wires=0)
-    qml.RY(x[1], wires=1)
-    #Ansazt
-    for i in range(l):
-        qml.RY(theta[i][0], wires=0)
-        qml.RY(theta[i][1],wires=1)
-        qml.CNOT(wires=[0,1])
-    return qml.expval(qml.PauliZ(0))
+    def circuito(x, theta):
+        l=len(theta)
+        #Encoding
+        for i in range(n_qubits):
+            qml.RY(x[i], wires=i)
+        #Ansazt
+        for i in range(l):
+            for j in range(n_qubits):
+                qml.RY(theta[i][j], wires=j)
+            for j in range (n_qubits-1):
+                qml.CNOT(wires=[j,j+1]) 
+        return qml.expval(qml.PauliZ(0))
+    return circuito
 
